fab28d99b432005188d8a2755943823c4fd52a4b
Tabitha Wong <tabi159w@gmail.com>
2023-02-02 17:13:58 -0500

    webscraper!!!

--- webscraper.py
+++ webscraper.py
@@ -17,8 +17,8 @@ for boat_make in makes[makes['Type'] == "Boat"]['Make']:
     # key: boat make as a string
     # value: list of url strings
 url_dict = {}
-make_urls = []
 for boat in boat_makes_list:
+    make_urls = []
     clean_boat = boat.replace("&", "and")
     final_boat = clean_boat.replace(" ", "-")
     for year in range(2010,2023):
@@ -26,30 +26,46 @@ for boat in boat_makes_list:
         # do a sleep
         url = "https://www.jdpower.com/boats/" + str(year) + "/" + final_boat.lower()
         make_urls.append(url)
+    # setting the dictionary value to be the list of URLs
     url_dict[boat] = make_urls
 
 # iterating through the makes in the dictionary
 for make in url_dict:
-    # iterating through the  
+    make_name = make
+    makes_list = []
+    models_list = []
+    # iterating through the URLs in each list 
     for url in url_dict[make]:
         URL = url
-        print(URL)
         time.sleep(3)
         page = requests.get(URL)
-        # setting up the soup (parsing through the html on the URL)
         soup = BeautifulSoup(page.content, "html.parser")
-        # finding all elements that have the specified class in the soup
-        results = soup.find_all('a', attrs={"class":"detail-row__link detail-row__link--fixed-width"})
-        models_list = []
-        # grabbing all the names
-        for result in results:
-            # cleaning up the results and removing \r\n
-            raw_model = result.text
-            # removing first \r\n
-            index_model = raw_model.replace("\r\n                                        ","")
-            # removing second \r\n
-            split_index = (index_model.index("\r\n"))
-            model = index_model[0:split_index]
-            # adding model to the list
-            models_list.append(model)
-        print(models_list)
+        # checking if page exists on JD Power     
+        if (len(soup.find_all('h1', string="Sorry, We Couldn't Find That Page")) > 0):
+            # skip if the page doesn't exist
+            print(URL)
+            continue
+        else: 
+            # setting up the soup (parsing through the html on the URL)
+            results = soup.find_all('a', attrs={"class":"detail-row__link detail-row__link--fixed-width"})
+            # grabbing all the model names
+            #for result in results:
+            for result in results:
+                # cleaning up the results and removing \r\n
+                raw_model = result.text
+                # removing first \r\n
+                index_model = raw_model.replace("\r\n                                        ","")
+                # removing second \r\n
+                split_index = (index_model.index("\r\n"))
+                model = index_model[0:split_index]
+                # adding to a list of makes and models to make writing to Excel easier
+                makes_list.append(make_name.upper())
+                models_list.append(model.upper())
+    # names of columns
+    names = ['Make','Model']
+    # making dataframe and writing to Excel
+    df = pd.DataFrame(list(zip(makes_list, models_list)), columns=names)
+    print(df)
+    df.to_excel("models.xlsx")
+
+print("DONE!")
